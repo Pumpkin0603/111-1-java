9cccb7d00aba3b4212858f03497d2f62b7724cbf
Pumpkin0603 <114204162+Pumpkin0603@users.noreply.github.com>
2022-09-23 11:41:47 +0800

    ROUND 3

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test{
     public static void main(String[] args){
         System.out.println(args[0]);
-        System.out.println("Second Versionl");
+        System.out.println("Second Version!");
     }
 }
