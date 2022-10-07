6979f76cb232b1226c36b5dbe4cab49e85a64160
Pumpkin0603 <114204162+Pumpkin0603@users.noreply.github.com>
2022-10-07 09:07:30 +0800

    2022/10/07

---
+++ Test6.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Test6{
+    public static void main(String[] args){
+        int grade;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Emter first integer:");
+        grade = input.nextInt();
+        
+          switch(grade /10){
+           
+            case 9:
+            case 10:
+                System.out.println("A");
+                break;
+            case 8:
+                System.out.println("B");
+                break;
+            case 7:
+                System.out.println("C");
+                break;
+            case 6:
+                System.out.println("D");
+                break;
+            default:
+                System.out.println("E");
+                break;
+        }
+    }
+}
